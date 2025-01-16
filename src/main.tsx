c5a621ad53307479d7ccb78216be6461a4e3638f
Nothingg <nothing9537@gmail.com>
2025-01-15 22:41:37 -0600

    base application + theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ThemeProvider } from '@/app/providers/theme-provider';
+import { App } from '@/app/App';
+
+import '@/app/styles/index.css';
+import { BuildComponentsTree } from '@/shared/components/BuildComponentsTree';
+
+const ComponentsTree = [StrictMode, ThemeProvider, App];
+
+createRoot(document.getElementById('root')!).render(BuildComponentsTree(ComponentsTree));
---
+++ src/shared/components/BuildComponentsTree.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-no-useless-fragment */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const BuildComponentsTree = (components: any[], currentIndex = 0) => {
+  if (currentIndex === components.length) {
+    return <></>;
+  }
+
+  const CurrentComponent = components[currentIndex];
+
+  return (
+    <CurrentComponent>
+      {BuildComponentsTree(components, currentIndex + 1)}
+    </CurrentComponent>
+  );
+};
